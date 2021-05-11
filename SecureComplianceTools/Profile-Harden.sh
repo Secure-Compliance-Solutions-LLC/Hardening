98917775573f604295d0a6e6d7a5ea8860337c2e
Austin Songer <austinvernsonger@protonmail.com>
2021-05-11 13:05:53 -0500

    Create Profile-Harden.sh

---
+++ SecureComplianceTools/Profile-Harden.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#
+#
+######################################
+# Testing if root...
+if [ $UID -ne 0 ]
+then
+    RED "You must run this script as root!" && echo
+    exit
+fi
+
+clear
+echo "Disabling kernel dumps and setting timeout to 1 hour"
+echo "ulimit -c 0 > /dev/null 2>&1" > /etc/profile.d/disable-coredumps.sh
+echo "TMOUT=3600" >> /etc/profile
+echo "readonly TMOUT" >> /etc/profile
+echo "export TMOUT" >> /etc/profile
+echo "* hard core 0" >> /etc/security/limits.conf
+echo "* soft core 0" >> /etc/security/limits.conf
+echo "fs.suid_dumpable=0" >> /etc/sysctl.conf
+echo "kernel.kptr_restrict=2" >> /etc/sysctl.conf
+sysctl -p > /dev/null
+
