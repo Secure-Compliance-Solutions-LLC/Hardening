8ef3e1d88625bbf2628b08b1bb5fa4dc58fe594c
Austin Songer <austinvernsonger@protonmail.com>
2021-05-11 13:00:39 -0500

    Update ParrotSec.sh

--- SecureComplianceTools/ParrotSec.sh
+++ SecureComplianceTools/ParrotSec.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
+#
+#
+#
+######################################
+
 # Testing if root...
 if [ $UID -ne 0 ]
 then
     RED "You must run this script as root!" && echo
     exit
 fi
+
+
+
