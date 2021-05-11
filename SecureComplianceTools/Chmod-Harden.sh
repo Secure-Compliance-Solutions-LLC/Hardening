74529f6215dec95a6bb46eb4ba59825b600d67fc
Austin Songer <austinvernsonger@protonmail.com>
2021-05-11 13:06:57 -0500

    Create Chmod-Harden.sh

---
+++ SecureComplianceTools/Chmod-Harden.sh
@@ -0,0 +1,16 @@
+
+
+clear
+echo "Restricting access to specific files and folders"
+chmod 600 /boot/grub/grub.cfg > /dev/null
+chmod 600 /etc/at.deny > /dev/null
+chmod 600 /etc/crontab > /dev/null
+chmod 600 /etc/ssh/sshd_config > /dev/null
+chmod 700 /etc/cron.d > /dev/null
+chmod 700 /etc/cron.daily > /dev/null
+chmod 700 /etc/cron.hourly > /dev/null
+chmod 700 /etc/cron.weekly > /dev/null
+chmod 700 /etc/cron.monthly > /dev/null
+chmod o-x /usr/bin/x86_64-linux-gnu-as > /dev/null
+chmod o-x /usr/bin/x86_64-linux-gnu-gcc-9 > /dev/null
+echo "Please ignore if you saw an error. That is because the compiler is not installed and does not need harden."
